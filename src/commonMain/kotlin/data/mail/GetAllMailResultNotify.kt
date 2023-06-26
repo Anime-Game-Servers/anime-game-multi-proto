1360d98ad5705ab266167b5d81f620e067341f98
hartie95 <mail@hartie95.de>
2023-06-27 00:26:48 +0200

    [Mail] Fixed another small oversight

--- src/commonMain/kotlin/data/mail/GetAllMailResultNotify.kt
+++ src/commonMain/kotlin/data/mail/GetAllMailResultNotify.kt
@@ -6,7 +6,7 @@ import org.anime_game_servers.annotations.ProtoCommand
 @AddedIn(messages.VERSION.V3_0_0)
 @ProtoCommand(org.anime_game_servers.annotations.CommandType.NOTIFY)
 interface GetAllMailResultNotify {
-    var mailList: List<MailItem>
+    var mailList: List<MailData>
     var transaction: String
     var totalPageCount: Int
     var pageIndex: Int
