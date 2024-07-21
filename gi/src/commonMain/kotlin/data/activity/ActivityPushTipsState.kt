6a45cbe89682bc25543b24db9a21f8012d9f53bc
hartie95 <mail@hartie95.de>
2024-07-21 08:16:39 +0200

    [GI/Proto] Added annotation to properly mark some protos added in version * ActivityPushTipsData * ActivityPushTipsState * OfficialCustomDungeon

--- gi/src/commonMain/kotlin/data/activity/ActivityPushTipsState.kt
+++ gi/src/commonMain/kotlin/data/activity/ActivityPushTipsState.kt
@@ -1,7 +1,10 @@
 package data.activity
 
+import org.anime_game_servers.core.base.Version.GI_2_8_0
+import org.anime_game_servers.core.base.annotations.AddedIn
 import org.anime_game_servers.core.base.annotations.proto.ProtoEnum
 
+@AddedIn(GI_2_8_0)
 @ProtoEnum
 enum class ActivityPushTipsState {
     ACTIVITY_PUSH_TIPS_STATE_NONE,
