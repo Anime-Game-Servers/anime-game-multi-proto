25dcf70d8a81eb3bcc6b4b56baa09ff0918c4d90
hartie95 <mail@hartie95.de>
2024-08-19 16:29:32 +0200

    [GI/Proto] Added models for drafts

---
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/gallery/draft/DraftInviteFailReason.kt
@@ -0,0 +1,23 @@
+package org.anime_game_servers.multi_proto.gi.data.gallery.draft
+
+import org.anime_game_servers.core.base.Version.GI_1_5_0
+import org.anime_game_servers.core.base.annotations.AddedIn
+import org.anime_game_servers.core.base.annotations.proto.AltName
+import org.anime_game_servers.core.base.annotations.proto.ProtoEnum
+
+@AddedIn(GI_1_5_0)
+@ProtoEnum
+internal enum class DraftInviteFailReason {
+    @AltName("DRAFT_INVITE_FAIL_REASON_UNKNOWN")
+    DRAFT_FAIL_UNKNOWN,
+    @AltName("DRAFT_INVITE_FAIL_REASON_ACTIVITY_NOT_OPEN")
+    DRAFT_ACTIVITY_NOT_OPEN,
+    @AltName("DRAFT_INVITE_FAIL_REASON_ACTIVITY_PLAY_NOT_OPEN")
+    DRAFT_ACTIVITY_PLAY_NOT_OPEN,
+    @AltName("DRAFT_INVITE_FAIL_REASON_SCENE_NOT_MEET")
+    DRAFT_SCENE_NOT_MEET,
+    @AltName("DRAFT_INVITE_FAIL_REASON_WORLD_NOT_MEET")
+    DRAFT_WORLD_NOT_MEET,
+    @AltName("DRAFT_INVITE_FAIL_REASON_PLAY_LIMIT_NOT_MEET")
+    DRAFT_PLAY_LIMIT_NOT_MEET,
+}
---
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/gallery/draft/DraftOwnerStartInviteRsp.kt
@@ -0,0 +1,17 @@
+package org.anime_game_servers.multi_proto.gi.data.gallery.draft
+
+import org.anime_game_servers.core.base.Version.GI_1_4_0
+import org.anime_game_servers.core.base.Version.GI_1_5_0
+import org.anime_game_servers.core.base.annotations.AddedIn
+import org.anime_game_servers.core.base.annotations.proto.CommandType.*
+import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
+
+@AddedIn(GI_1_4_0)
+@ProtoCommand(RESPONSE)
+internal interface DraftOwnerStartInviteRsp {
+    var draftId: Int
+    var wrongUid: Int
+    var retcode: Int
+    @AddedIn(GI_1_5_0)
+    var inviteFailInfoList: List<DraftInviteFailInfo>
+}
