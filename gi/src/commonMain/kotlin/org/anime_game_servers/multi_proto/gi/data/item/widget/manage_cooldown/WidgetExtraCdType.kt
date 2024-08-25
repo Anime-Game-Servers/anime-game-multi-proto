e65902bfd10c52054d3c090c68d501f9639aecb2
hartie95 <mail@hartie95.de>
2024-08-26 01:55:07 +0200

    [GI/Fix] Added a missing altName for `WidgetExtraCdType`

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/item/widget/manage_cooldown/WidgetExtraCdType.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/item/widget/manage_cooldown/WidgetExtraCdType.kt
@@ -5,7 +5,7 @@ import org.anime_game_servers.core.base.annotations.AddedIn
 import org.anime_game_servers.core.base.annotations.proto.ProtoEnum
 
 @AddedIn(GI_2_3_0)
-@ProtoEnum(alternativeNames = ["WidgetExtraCDType", "WIDGET_EXTRA_CD_TYPE"])
+@ProtoEnum(alternativeNames = ["WidgetExtraCDType", "WIDGET_EXTRA_CD_TYPE", "WIDGETEXTRACDTYPE"])
 internal enum class WidgetExtraCdType {
     WIDGET_EXTRA_CD_TYPE_NONE,
     WIDGET_EXTRA_CD_TYPE_E_SKILL_SHARED,
