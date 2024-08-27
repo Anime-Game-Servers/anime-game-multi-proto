100f101889867bcf92646c096c3595c58e4e9cb7
hartie95 <mail@hartie95.de>
2024-08-27 02:05:09 +0200

    [GI/Refactoring] Remove the duplicated widget models

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/WorldPlayerInfoNotify.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/WorldPlayerInfoNotify.kt
@@ -1,13 +1,13 @@
 package org.anime_game_servers.multi_proto.gi.data.scene
 
 import org.anime_game_servers.core.base.annotations.AddedIn
-import org.anime_game_servers.multi_proto.gi.data.general.widget.PlayerWidgetInfo
 import org.anime_game_servers.multi_proto.gi.data.scene.entity.OnlinePlayerInfo
 import org.anime_game_servers.core.base.annotations.proto.CommandType
 import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
 
 import org.anime_game_servers.core.base.Version.GI_CB2
 import org.anime_game_servers.core.base.Version.GI_3_0_0
+import org.anime_game_servers.multi_proto.gi.data.item.widget.PlayerWidgetInfo
 
 @AddedIn(GI_CB2)
 @ProtoCommand(CommandType.NOTIFY)
