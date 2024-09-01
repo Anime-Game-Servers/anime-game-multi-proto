e8ae7b887a7abe60cd78c4633182733d1dda281d
hartie95 <mail@hartie95.de>
2024-09-01 23:27:56 +0200

    [GI/Proto] Added some mising info from newer versions to Retcode and SceneGadgetInfo

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/entity/SceneGadgetInfo.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/entity/SceneGadgetInfo.kt
@@ -1,5 +1,6 @@
 package org.anime_game_servers.multi_proto.gi.data.scene.entity
 
+import org.anime_game_servers.core.base.Version.*
 import org.anime_game_servers.core.base.annotations.AddedIn
 import org.anime_game_servers.multi_proto.gi.data.general.CustomGadgetTreeInfo
 import org.anime_game_servers.multi_proto.gi.data.general.item.Item
@@ -8,21 +9,10 @@ import org.anime_game_servers.core.base.annotations.proto.OneOf
 import org.anime_game_servers.core.base.annotations.proto.OneOfEntry
 import org.anime_game_servers.core.base.annotations.proto.OneOfType
 import org.anime_game_servers.core.base.annotations.proto.ProtoModel
-
-import org.anime_game_servers.core.base.Version.GI_CB1
-import org.anime_game_servers.core.base.Version.GI_CB2
-import org.anime_game_servers.core.base.Version.GI_0_9_0
-import org.anime_game_servers.core.base.Version.GI_1_0_0
-import org.anime_game_servers.core.base.Version.GI_1_1_0
-import org.anime_game_servers.core.base.Version.GI_1_2_0
-import org.anime_game_servers.core.base.Version.GI_1_3_0
-import org.anime_game_servers.core.base.Version.GI_1_4_0
-import org.anime_game_servers.core.base.Version.GI_1_6_0
-import org.anime_game_servers.core.base.Version.GI_2_0_0
-import org.anime_game_servers.core.base.Version.GI_2_1_0
-import org.anime_game_servers.core.base.Version.GI_2_2_0
-import org.anime_game_servers.core.base.Version.GI_2_8_0
-import org.anime_game_servers.core.base.Version.GI_3_8_0
+import org.anime_game_servers.multi_proto.gi.data.activity.echo_shell.EchoShellInfo
+import org.anime_game_servers.multi_proto.gi.data.gadget.BlossomChestInfo
+import org.anime_game_servers.multi_proto.gi.data.gadget.FoundationInfo
+import org.anime_game_servers.multi_proto.gi.data.gadget.GadgetGeneralRewardInfo
 
 @AddedIn(GI_CB1)
 @ProtoModel
@@ -55,7 +45,7 @@ internal interface SceneGadgetInfo {
     var playInfo: GadgetPlayInfo
 
     @OneOf(
-        OneOfEntry(Item::class, "trifle_item"),
+        OneOfEntry(Item::class, "trifle_item", removedIn = GI_4_0_0),
         OneOfEntry(Int::class, "item_id", removedIn = GI_CB2),
         OneOfEntry(GatherGadgetInfo::class, "gather_gadget", addedIn = GI_CB2),
         OneOfEntry(WorktopInfo::class, "worktop"),
@@ -65,19 +55,21 @@ internal interface SceneGadgetInfo {
         OneOfEntry(AbilityGadgetInfo::class, "ability_gadget"),
         OneOfEntry(StatueGadgetInfo::class, "statue_gadget", addedIn = GI_CB2),
         OneOfEntry(BossChestInfo::class, "boss_chest", addedIn = GI_0_9_0),
-        OneOfEntry(org.anime_game_servers.multi_proto.gi.data.gadget.BlossomChestInfo::class, "blossom_chest", addedIn = GI_0_9_0),
+        OneOfEntry(BlossomChestInfo::class, "blossom_chest", addedIn = GI_0_9_0),
         OneOfEntry(MpPlayRewardInfo::class, "mp_play_reward", addedIn = GI_0_9_0),
-        OneOfEntry(org.anime_game_servers.multi_proto.gi.data.gadget.GadgetGeneralRewardInfo::class, "general_reward", addedIn = GI_1_1_0),
+        OneOfEntry(GadgetGeneralRewardInfo::class, "general_reward", addedIn = GI_1_1_0),
         OneOfEntry(OfferingInfo::class, "offering_info", addedIn = GI_1_2_0),
-        OneOfEntry(org.anime_game_servers.multi_proto.gi.data.gadget.FoundationInfo::class, "foundation_info", addedIn = GI_1_3_0),
+        OneOfEntry(FoundationInfo::class, "foundation_info", addedIn = GI_1_3_0),
         OneOfEntry(VehicleInfo::class, "vehicle_info", addedIn = GI_1_6_0),
-        OneOfEntry(org.anime_game_servers.multi_proto.gi.data.activity.echo_shell.EchoShellInfo::class, "shell_info", addedIn = GI_1_6_0),
+        OneOfEntry(EchoShellInfo::class, "shell_info", addedIn = GI_1_6_0),
         OneOfEntry(ScreenInfo::class, "screen_info", addedIn = GI_2_0_0),
         OneOfEntry(FishPoolInfo::class, "custom_gadget_tree_info", addedIn = GI_2_1_0),
         OneOfEntry(CustomGadgetTreeInfo::class, "custom_gadget_tree_info", addedIn = GI_2_2_0),
         OneOfEntry(RoguelikeGadgetInfo::class, "roguelike_gadget_info", addedIn = GI_2_2_0),
         OneOfEntry(NightCrowGadgetInfo::class, "night_crow_gadget_info", addedIn = GI_2_8_0),
-        OneOfEntry(DeshretObeliskGadgetInfo::class, "deshret_obelisk_gadget_info", addedIn = GI_3_8_0),
+        OneOfEntry(DeshretObeliskGadgetInfo::class, "deshret_obelisk_gadget_info", addedIn = GI_3_0_0),
+        OneOfEntry(CoinCollectOperatorInfo::class, "coin_collect_operator_info", addedIn = GI_3_3_0),
+        OneOfEntry(TrifleGadget::class, "trifle_gadget", addedIn = GI_4_0_0),
     )
     var content: OneOfType
 }
