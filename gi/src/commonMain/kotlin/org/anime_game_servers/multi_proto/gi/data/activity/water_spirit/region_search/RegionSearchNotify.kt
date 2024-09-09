b847f280b1159ac379f0df41082d53abc24d190d
hartie95 <mail@hartie95.de>
2024-09-09 22:12:05 +0200

    [GI/Proto] Added "Wishful Drops"/Water_Spirit_Challenge related packets

---
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/activity/water_spirit/region_search/RegionSearchNotify.kt
@@ -0,0 +1,13 @@
+package org.anime_game_servers.multi_proto.gi.data.activity.water_spirit.region_search
+
+import org.anime_game_servers.core.base.Version.GI_1_4_0
+import org.anime_game_servers.core.base.annotations.AddedIn
+import org.anime_game_servers.core.base.annotations.proto.CommandType.*
+import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
+
+@AddedIn(GI_1_4_0)
+@ProtoCommand(NOTIFY)
+internal interface RegionSearchNotify {
+    var regionSearchList: List<RegionSearchInfo>
+    var uid: Int
+}
