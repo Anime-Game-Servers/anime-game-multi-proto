2c91555b3e952670971cfd2dafa9fda6c6d6b34a
hartie95 <mail@hartie95.de>
2024-09-09 20:21:00 +0200

    [GI/Fix] Fixed name or MapMarkFromType none

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/map/MapMarkFromType.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/map/MapMarkFromType.kt
@@ -8,8 +8,8 @@ import org.anime_game_servers.core.base.annotations.proto.ProtoEnum
 @AddedIn(GI_CB2)
 @ProtoEnum
 internal enum class MapMarkFromType {
-    @AltName("MAP_MARK_FROM_TYPE_NONE")
-    MAP_MARK_FROM_TYPE_NOE,
+    @AltName("MAP_MARK_FROM_TYPE_NOE")
+    MAP_MARK_FROM_TYPE_NONE,
     MAP_MARK_FROM_TYPE_MONSTER,
     MAP_MARK_FROM_TYPE_QUEST,
 }
