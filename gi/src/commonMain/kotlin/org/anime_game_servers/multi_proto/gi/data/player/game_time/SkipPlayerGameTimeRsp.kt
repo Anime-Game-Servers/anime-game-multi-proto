c0cf939d2ed7ad9983d37f9049975101c93a808a
hartie95 <mail@hartie95.de>
2024-12-27 20:54:26 +0100

    [GI/Fix] Fixed version SkipPlayerGameTimeReq/Rsp got added

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/player/game_time/SkipPlayerGameTimeRsp.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/player/game_time/SkipPlayerGameTimeRsp.kt
@@ -1,12 +1,12 @@
 package org.anime_game_servers.multi_proto.gi.data.player.game_time
 
 import org.anime_game_servers.core.base.annotations.AddedIn
-import org.anime_game_servers.core.base.Version.GI_3_4_0
+import org.anime_game_servers.core.base.Version.GI_3_5_0
 import org.anime_game_servers.core.base.annotations.proto.CommandType.RESPONSE
 import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
 import org.anime_game_servers.multi_proto.gi.data.general.Retcode
 
-@AddedIn(GI_3_4_0)
+@AddedIn(GI_3_5_0)
 @ProtoCommand(RESPONSE)
 internal interface SkipPlayerGameTimeRsp {
     var retCode: Retcode
