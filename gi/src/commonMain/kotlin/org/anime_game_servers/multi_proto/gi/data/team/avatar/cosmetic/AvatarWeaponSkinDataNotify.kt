8feb33dd363249007368d27ab945ab7a7978a5b2
osddeitf <osddeitf@gmail.com>
2025-12-03 15:56:31 +0700

    update

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/team/avatar/cosmetic/AvatarWeaponSkinDataNotify.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/team/avatar/cosmetic/AvatarWeaponSkinDataNotify.kt
@@ -1,12 +1,12 @@
 package org.anime_game_servers.multi_proto.gi.data.team.avatar.cosmetic
 
-import org.anime_game_servers.core.base.Version
+import org.anime_game_servers.core.base.Version.GI_5_7_0
 import org.anime_game_servers.core.base.annotations.AddedIn
-import org.anime_game_servers.core.base.annotations.proto.CommandType
+import org.anime_game_servers.core.base.annotations.proto.CommandType.NOTIFY
 import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
 
-@AddedIn(Version.GI_5_7_0)
-@ProtoCommand(CommandType.NOTIFY)
+@AddedIn(GI_5_7_0)
+@ProtoCommand(NOTIFY)
 internal interface AvatarWeaponSkinDataNotify {
     var skinInfoList: List<WeaponSkinInfo>
     var skinIdList: List<Int>
--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/team/avatar/cosmetic/AvatarWearWeaponSkinReq.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/team/avatar/cosmetic/AvatarWearWeaponSkinReq.kt
@@ -1,11 +1,11 @@
 package org.anime_game_servers.multi_proto.gi.data.team.avatar.cosmetic
 
-import org.anime_game_servers.core.base.Version
+import org.anime_game_servers.core.base.Version.GI_5_7_0
 import org.anime_game_servers.core.base.annotations.AddedIn
-import org.anime_game_servers.core.base.annotations.proto.CommandType.*
+import org.anime_game_servers.core.base.annotations.proto.CommandType.REQUEST
 import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
 
-@AddedIn(Version.GI_5_7_0)
+@AddedIn(GI_5_7_0)
 @ProtoCommand(REQUEST)
 internal interface AvatarWearWeaponSkinReq {
     var avatarGuidList: List<Long>
