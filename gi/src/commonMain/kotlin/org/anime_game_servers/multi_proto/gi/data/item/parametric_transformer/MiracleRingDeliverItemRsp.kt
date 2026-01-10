540c0fa2bd0a257468ef9c5735188e2358c6268c
hartie95 <mail@hartie95.de>
2026-01-10 15:42:15 +0100

    [GI/Proto] Added models for the packets used for the parametric transformer

---
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/item/parametric_transformer/MiracleRingDeliverItemRsp.kt
@@ -0,0 +1,19 @@
+package org.anime_game_servers.multi_proto.gi.data.item.parametric_transformer
+
+import org.anime_game_servers.core.base.Version.GI_1_3_0
+import org.anime_game_servers.core.base.annotations.AddedIn
+import org.anime_game_servers.core.base.annotations.proto.CommandType.*
+import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
+import org.anime_game_servers.multi_proto.gi.data.gadget.InterOpType
+import org.anime_game_servers.multi_proto.gi.data.gadget.InteractType
+import org.anime_game_servers.multi_proto.gi.data.general.Retcode
+
+@AddedIn(GI_1_3_0)
+@ProtoCommand(RESPONSE)
+internal interface MiracleRingDeliverItemRsp {
+    var retcode: Retcode
+    var gadgetEntityId: Int
+    var gadgetId: Int
+    var interactType: InteractType
+    var opType: InterOpType
+}
---
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/item/parametric_transformer/UseMiracleRingReq.kt
@@ -0,0 +1,15 @@
+package org.anime_game_servers.multi_proto.gi.data.item.parametric_transformer
+
+import org.anime_game_servers.core.base.Version.GI_1_3_0
+import org.anime_game_servers.core.base.annotations.AddedIn
+import org.anime_game_servers.core.base.annotations.proto.CommandType.*
+import org.anime_game_servers.core.base.annotations.proto.ProtoCommand
+import org.anime_game_servers.multi_proto.gi.data.general.Vector
+
+@AddedIn(GI_1_3_0)
+@ProtoCommand(REQUEST)
+internal interface UseMiracleRingReq {
+    var miracleRingOpType: MiracleRingOpType
+    var pos: Vector
+    var rot: Vector
+}
