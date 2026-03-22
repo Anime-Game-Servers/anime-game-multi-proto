1696d88526ba0eb341191789feb66609ba66218c
hartie95 <mail@hartie95.de>
2026-03-22 10:11:59 +0100

    [Refactoring] Added missing AddedIn Annotation and applied conventions (retcode first, in version sorted alphabetically)

--- gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/map/MapAreaInfo.kt
+++ gi/src/commonMain/kotlin/org/anime_game_servers/multi_proto/gi/data/scene/map/MapAreaInfo.kt
@@ -1,9 +1,12 @@
 package org.anime_game_servers.multi_proto.gi.data.scene.map
 
+import org.anime_game_servers.core.base.Version.GI_2_2_0
+import org.anime_game_servers.core.base.annotations.AddedIn
 import org.anime_game_servers.core.base.annotations.proto.ProtoModel
 
+@AddedIn(GI_2_2_0)
 @ProtoModel
 internal interface MapAreaInfo {
-    var mapAreaId: Int
     var isOpen: Boolean
+    var mapAreaId: Int
 }
