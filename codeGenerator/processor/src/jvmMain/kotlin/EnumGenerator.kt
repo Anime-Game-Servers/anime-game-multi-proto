d25372238c0e9b66f03d86f8e9ff4b2213a8b3fd
hartie95 <mail@hartie95.de>
2026-04-20 12:56:46 +0200

    [codegen] added spliting for to bug enums, like Retcodes

--- codeGenerator/processor/src/jvmMain/kotlin/EnumGenerator.kt
+++ codeGenerator/processor/src/jvmMain/kotlin/EnumGenerator.kt
@@ -1,10 +1,8 @@
 import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.getAnnotationsByType
-import com.google.devtools.ksp.getDeclaredProperties
 import com.google.devtools.ksp.processing.KSPLogger
 import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.symbol.KSClassDeclaration
-import com.google.devtools.ksp.symbol.KSPropertyDeclaration
 import com.google.devtools.ksp.symbol.KSType
 import org.anime_game_servers.core.base.annotations.proto.AltName
 import java.io.OutputStream
@@ -12,6 +10,7 @@ import kotlin.collections.addAll
 import kotlin.sequences.forEach
 
 const val UNRECOGNISED_ENUM_NAME = "UNRECOGNISED"
+const val ENUM_SPLIT_LIMIT = 2000
 
 class EnumGenerator(
     logger: KSPLogger,
@@ -69,6 +68,7 @@ class EnumGenerator(
         return names
     }
 
+
     override fun addParsingMethods(file: OutputStream, classInfo: ClassInfo) {
         file.id(8) +="@JvmStatic\n"
         file.id(8) +="fun parseBy(value: Int, version:$VERSION_ENUM_CLASS_NAME): ${classInfo.name} {\n"
@@ -85,26 +85,69 @@ class EnumGenerator(
             val functionName = proto.parseFunctionName
             val protoClass = proto.absoluteClassName
             val protoMembers = proto.members
+
             file.id(8) += "internal fun $functionName(value:Int) : ${classInfo.name} {\n"
             file.id(12) +="return $functionName($protoClass.fromValue(value))\n"
             file.id(8) +="}\n"
 
-            file.id(8) +="internal fun $functionName(proto:$protoClass) : ${classInfo.name} {\n"
-            //file.id(12) +="return proto.name?.let {\n"
-            //file.id(16) +="${classInfo.name}.valueOf(it)\n"
-            //file.id(12) += "} ?: $UNRECOGNISED_ENUM_NAME\n"
-            file.id(12) +="return when(proto.name) {\n"
-            classInfo.declarations.forEach enumDeclaration@{ enumDeclaration ->
-                val names = enumDeclaration.getEnumNames()
-                if(names.isEmpty()) return@enumDeclaration
-                file.id(16) += names.joinToString(", ") { "\"$it\"" } +
-                " -> ${enumDeclaration.simpleName.asString()}\n"
+
+            if(classInfo.declarations.size > ENUM_SPLIT_LIMIT){
+                addLengthSplitEnumMapping(file, classInfo, functionName, protoClass)
+            } else {
+                addDirectEnumMapping(file, classInfo, functionName, protoClass)
             }
-            file.id(16) +="else -> UNRECOGNISED\n"
-            file.id(12) +="}\n"
-            file.id(8) +="}\n"
+
             file.id(8) +="\n"
         }
         file.id(4) +="}\n"
+    }
+
+    fun addDirectEnumMapping(file: OutputStream, classInfo: ClassInfo, functionName: String, protoClass : String){
+        file.id(8) +="internal fun $functionName(proto:$protoClass) : ${classInfo.name} {\n"
+        file.id(12) +="return when(proto.name) {\n"
+        classInfo.declarations.forEach enumDeclaration@{ enumDeclaration ->
+            val names = enumDeclaration.getEnumNames()
+            if(names.isEmpty()) return@enumDeclaration
+            file.id(16) += names.joinToString(", ") { "\"$it\"" } +
+                    " -> ${enumDeclaration.simpleName.asString()}\n"
+        }
+        file.id(16) +="else -> $UNRECOGNISED_ENUM_NAME\n"
+        file.id(12) +="}\n"
+        file.id(8) +="}\n"
+    }
+
+    fun addLengthSplitEnumMapping(file: OutputStream, classInfo: ClassInfo, functionName: String, protoClass : String){
+
+        val splitMap: MutableMap<Int, MutableMap<String, String>> = mutableMapOf()
+
+        classInfo.declarations.forEach enumDeclaration@{ enumDeclaration ->
+            val enumName = enumDeclaration.simpleName.asString()
+            enumDeclaration.getEnumNames().forEach { name ->
+                splitMap.computeIfAbsent(name.length, {mutableMapOf<String, String>()})[name] = enumName
+            }
+        }
+
+        file.id(8) +="internal fun $functionName(proto:$protoClass) : ${classInfo.name} {\n"
+        file.id(12) +="val name = proto.name ?: return $UNRECOGNISED_ENUM_NAME\n"
+        file.id(12) +="return when(name.length) {\n"
+        splitMap.forEach { (length, _) ->
+            file.id(16) += "$length -> ${functionName}_$length(name)\n"
+        }
+        file.id(16) +="else -> $UNRECOGNISED_ENUM_NAME\n"
+        file.id(12) +="}\n"
+        file.id(8) +="}\n"
+
+
+        splitMap.forEach { (length,enumMap) ->
+            file.id(8) += "internal fun ${functionName}_$length(name: String) : ${classInfo.name} {\n"
+
+            file.id(12) +="return when(name) {\n"
+            enumMap.forEach enumDeclaration@{ (protoName, enumName) ->
+                file.id(16) += "\"$protoName\" -> $enumName\n"
+            }
+            file.id(16) +="else -> $UNRECOGNISED_ENUM_NAME\n"
+            file.id(12) +="}\n"
+            file.id(8) +="}\n"
+        }
     }
 }
